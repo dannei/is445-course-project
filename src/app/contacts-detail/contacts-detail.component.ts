a93670bff51ca8553708a8f80724ff524c770eab
rjr4m <rjram.business@gmail.com>
2018-12-05 02:12:35 -0800

    Show details and delete contact

--- src/app/contacts-detail/contacts-detail.component.ts
+++ src/app/contacts-detail/contacts-detail.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, Router } from '@angular/router';
+import { FsService } from '../fs.service';
+
 @Component({
   selector: 'app-contacts-detail',
   templateUrl: './contacts-detail.component.html',
   styleUrls: ['./contacts-detail.component.css']
 })
 export class ContactsDetailComponent implements OnInit {
+  contact = {};
 
-  constructor() { }
+  getContactDetails(id) {
+    this.fs.getContact(id)
+      .subscribe(data => {
+        console.log(data);
+        this.contact = data;
+      });
+  }
+
+  deleteContact(id) {
+    this.fs.deleteContacts(id)
+      .subscribe(res => {
+          this.router.navigate(['/contacts']);
+        }, (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+  constructor(private route: ActivatedRoute, private router: Router, private fs: FsService) { }
 
   ngOnInit() {
+    this.getContactDetails(this.route.snapshot.params['id']);
   }
 
 }
