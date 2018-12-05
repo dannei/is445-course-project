85f273fa3ea763e5380f432a89b5f9b2742176f2
rjr4m <rjram.business@gmail.com>
2018-12-05 01:32:25 -0800

    Create angular 6 routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactsDetailComponent } from './contacts-detail/contacts-detail.component';
+import { ContactsCreateComponent } from './contacts-create/contacts-create.component';
+import { ContactsEditComponent } from './contacts-edit/contacts-edit.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+    data: { title: 'Contacts List' }
+  },
+  {
+    path: 'contacts-details/:id',
+    component: ContactsDetailComponent,
+    data: { title: 'Contacts Details' }
+  },
+  {
+    path: 'contacts-create',
+    component: ContactsCreateComponent,
+    data: { title: 'Create contacts' }
+  },
+  {
+    path: 'contacts-edit/:id',
+    component: ContactsEditComponent,
+    data: { title: 'Edit contacts' }
+  },
+  { path: '',
+    redirectTo: '/contacts',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactsDetailComponent,
+    ContactsCreateComponent,
+    ContactsEditComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
